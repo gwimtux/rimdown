954053853b5546e95c3333139bfb9f6971d77494
gwimtux <>
2023-08-21 16:38:13 -0700

    more updates with steamcmd downloader

--- main.py
+++ main.py
@@ -7,14 +7,24 @@ class RimDown:
     def __init__(self):
         self.setup()
         self.gameID = 294100
+        self.steamcmd_path = os.path.expanduser('~/.config/rimdown/steam/steamcmd.sh')
 
     def setup(self):
         try:
             self.mod_dir = open(os.path.expanduser('~/.config/rimdown/settings.cfg'), 'r').read()
         except FileNotFoundError:
+            steam_dir = os.path.expanduser('~/.config/rimdown/steam')
             config_dir = os.path.expanduser('~/.config/rimdown')
-            if not os.path.isdir(config_dir):
+
+            # download steamcmd if not installed
+            if not os.path.isdir(config_dir) and not os.path.isdir(steam_dir):
                 os.mkdir(config_dir)
+                os.mkdir(steam_dir)
+                os.chdir(steam_dir)
+                print('Downloading steamcmd...')
+                subprocess.call('curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf -', shell=True)
+                os.system('clear')
+                
             self.mod_dir = input('Enter the path to RimWorld mods folder: ')
             #check if path is valid
             if not os.path.isdir(self.mod_dir):
@@ -29,7 +39,7 @@ class RimDown:
         workshopID = mod.split('?id=')[1][:10]
 
     def download_mods(self):
-        subprocess.call([f'steamcmd', '+force_install_dir', self.mod_dir, '+login', 'anonymous', '+workshop_download_item', str(self.gameID), str(workshopID), '+quit'])
+        subprocess.call([self.steamcmd_path, '+force_install_dir', self.mod_dir, '+login', 'anonymous', '+workshop_download_item', str(self.gameID), str(workshopID), '+quit'])
 
     def extract_mods(self):
         tree = ET.parse(f'{self.mod_dir}/steamapps/workshop/content/{self.gameID}/{workshopID}/About/About.xml')
